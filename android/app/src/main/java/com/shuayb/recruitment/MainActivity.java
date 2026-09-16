1419d3f0937238cced943b6d7fc99f9011a38926
shuaib54454-debug <shuaib54454@gmail.com>
2026-09-16 22:34:53 +0300

    security: protect sensitive app content from screenshots

--- android/app/src/main/java/com/shuayb/recruitment/MainActivity.java
+++ android/app/src/main/java/com/shuayb/recruitment/MainActivity.java
@@ -1,5 +1,16 @@
 package com.shuayb.recruitment;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Candidate/passport data is sensitive: prevent screenshots, screen recording,
+        // and exposure in the Android recent-apps preview.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
